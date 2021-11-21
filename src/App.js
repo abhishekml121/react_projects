e6a857f685dc275c76953b5a42952f06c1d3a6ae
abhishek kamal <abhishekmpoz@gmail.com>
2021-11-21 22:01:04 +0530

    Simple add user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import AddUser from './components/UI/Users/AddUser';
+import UsersList from './components/UI/Users/UsersList';
 
 
 function App() {
-  return (
-    <div>
+  const [usersList, setUsersList] = useState([]);
 
-    </div>
+  const addUserHandler = (user_name, user_age) => {
+    setUsersList((prevUsersList)=>{
+      return [{name:user_name, age:user_age, id:Math.random().toString()}, ...prevUsersList];
+    });
+
+  }
+  
+  return (
+    <>
+    <AddUser onAddUser={addUserHandler}/>
+    <UsersList users={usersList} />
+    </>
   );
 }
 
